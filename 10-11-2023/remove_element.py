d8686ca144041ffb39b74fb5813dcf9fd26c8d8d
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2023-11-12 21:41:32 +0100

    i go to bed

--- 10-11-2023/remove_element.py
+++ 10-11-2023/remove_element.py
@@ -1,8 +1,9 @@
 class Solution():
     def removeElement(self, nums, val):
-        counter = 0
+        position = 0
         for num in nums:
             if num != val:
-                nums[counter] = num
-                counter += 1
-        return counter
+                nums[position] = num
+                position += 1
+        return position
+
