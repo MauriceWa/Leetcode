2ebec82e900e4d5e58d7fc5f358e325a57f0bccd
MauriceWa <144155257+MauriceWa@users.noreply.github.com>
2023-11-18 12:47:13 +0100

    improved haystack leetcode

--- 17-11-2023/needle_haystack.py
+++ 17-11-2023/needle_haystack.py
@@ -1,9 +1,10 @@
 '''' if needle is in haystack(list) return 0, if needle is not in haystack return -1'''
 class Solution:
-    def strStr(self, haystack: str, needle: str):
-        if needle in haystack:
-            return 0
-        else:
-            return -1
-
-
+    class Solution:
+        def strStr(self, haystack: str, needle: str) -> int:
+            if not needle:
+                return 0
+            if needle in haystack:
+                return haystack.index(needle)
+            else:
+                return -1 
